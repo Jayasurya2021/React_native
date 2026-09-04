b1a7121470c9172ee49549af9226a6c967bd3e75
Jayasurya <61399363+jayasurya-developer@users.noreply.github.com>
2026-09-04 11:04:36 +0530

    Implement structural updates and optimizations across multiple modules

--- firstProject/src/app/index.jsx
+++ firstProject/src/app/index.jsx
@@ -1,9 +1,16 @@
-import { StyleSheet, Text, View } from 'react-native'
+import backgroundImage from "@/assets/images/background.jpg"
+import { ImageBackground, StyleSheet, Text, View } from 'react-native'
 
 const index = () => {
   return (
     <View style={style.container}>
-      <Text style={style.text}>hello</Text>
+      <ImageBackground
+        source={backgroundImage}
+        style={style.background}
+        resizeMode="cover"
+      >
+        <Text style={style.text}>Spider Man</Text>
+      </ImageBackground >
     </View>
   )
 }
@@ -16,9 +23,16 @@ const style = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
-  text:{
-    fontSize:40,
-    color: "green",
-    backgroundColor: "while"
+  background: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    height: "100%"
+  },
+  text: {
+    fontSize: 40,
+    color: "black",
+    fontFamily: "SpiderFont"
   }
 })
